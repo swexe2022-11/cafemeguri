3acabb1d4608558f8f4ad60748e2c02c581242fb
takushima <ubuntu@ip-172-31-24-105.ec2.internal>
2023-01-18 01:18:52 +0000

    push to render.com

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Cafemeguri
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.hosts << 'xxx.onrender.com'
   end
 end
